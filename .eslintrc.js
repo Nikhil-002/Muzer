8444094408f9ea7deef22025d9574e6b44d6bb4d
hello <nikhilk9350@gmail.com>
2025-06-09 03:09:42 +0530

    remove linting

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["next/core-web-vitals", "next"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-object-type": "off",
+  },
+};
